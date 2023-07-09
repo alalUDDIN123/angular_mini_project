e724291e54cc79882f2c19164eb4b9c3f5eab489
alalUDDIN123 <alalu75666@gmail.com>
2023-07-09 09:34:36 +0530

    mini_project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { TodoComponent } from './components/todo/todo.component';
-
-
-
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    TodoComponent
+ 
   ],
   imports: [
     BrowserModule,
