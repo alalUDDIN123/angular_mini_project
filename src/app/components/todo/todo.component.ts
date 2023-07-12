adfefde600d4c9ca738e6a92a39287832d353cda
alalUDDIN123 <alalu75666@gmail.com>
2023-07-12 06:48:54 +0530

    todo_crud_ngrx

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -10,32 +10,7 @@ import { Todo } from '../models/Todo.model';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent {
-  todos$: Todo[] = [
-    {
-      id: 1,
-      title: "Sample todo 1",
-      description: "This is a sample todo 1",
-      isCompleted: false
-    },
-    {
-      id: 2,
-      title: "Sample todo 2",
-      description: "This is a sample todo 2",
-      isCompleted: true
-    },
-    {
-      id: 3,
-      title: "Sample todo 3",
-      description: "This is a sample todo 3",
-      isCompleted: false
-    },
-    {
-      id: 4,
-      title: "Sample todo 4",
-      description: "This is a sample todo 4",
-      isCompleted: false
-    }
-  ];
+  
 
   showCreateForm = true;
   formError = false;
@@ -54,64 +29,20 @@ export class TodoComponent {
   };
 
   createTodo() {
-    if (this.newTodo.title.trim() === '' || this.newTodo.description.trim() === '') {
-      this.showError('Please fill in all fields.');
-      return;
-    }
-
-    const newId = this.todos$[this.todos$.length - 1].id + 1;
-    const newTodo: Todo = {
-      id: newId,
-      title: this.newTodo.title,
-      description: this.newTodo.description,
-      isCompleted: false
-    };
-    this.todos$.push(newTodo);
-
-    this.newTodo = {
-      id: 0,
-      title: '',
-      description: '',
-      isCompleted: false
-    };
-    this.formError = false;
+ 
   }
 
   showUpdateForm(index: number) {
-    this.showCreateForm = false;
-    const todoToUpdate = this.todos$[index];
-    this.updatedTodo = {
-      id: todoToUpdate.id,
-      title: todoToUpdate.title,
-      description: todoToUpdate.description,
-      isCompleted: todoToUpdate.isCompleted
-    };
+   
   }
 
   updateTodo() {
-    const index = this.todos$.findIndex(todo => todo.id === this.updatedTodo.id);
 
-    if (index !== -1) {
-      this.todos$[index].title = this.updatedTodo.title;
-      this.todos$[index].description = this.updatedTodo.description;
-    }
-
-    this.updatedTodo = {
-      id: 0,
-      title: '',
-      description: '',
-      isCompleted: false
-    };
-    this.showCreateForm = true;
   }
 
 
   deleteTodo(id: number) {
-    // console.log("delete item id:-", id);
-
-    const remainingTodo = this.todos$.filter((todo) => todo.id !== id);
-    // console.log("reset of the todos after delete one:-", remainingTodo);
-    this.todos$ = remainingTodo
+   
 
 
   }
